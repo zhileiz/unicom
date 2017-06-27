391410c459462f23b5997bc0aa24c537fda5f498
zhileiz <zhileiz@seas.upenn.edu>
2017-06-27 04:11:32 -0400

    repeat

--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,23 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
         Set<String> allStr = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
-        String[] urls = new String[2];
-        urls[0] = "http://www.dianping.com/search/around/1/0_2602612";
-        urls[1] = "http://www.dianping.com/search/around/1/0_8872865";
         ConcurrentHashMap<String,String> map = new ConcurrentHashMap<String,String>();
-        ArrayList<Thread> preThreads = new ArrayList<Thread>();
-        for (int i = 1; i < 3; i++) {
-            preThreads.add(new Thread(new PreThread(i,map)));
+        for (int i = 1; i < 28; i = i + 3) {
+            startPreThread(1, 2, map);
+            pushToMainThread(map, allStr);
         }
-        for (int i = 0; i < 2; i++) {
-            preThreads.get(i).start();
+    }
+
+    public static void printToCSV(String file, Set<String> strs) throws IOException{
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        Iterator<String> it = strs.iterator();
+        while (it.hasNext()){
+            writer.write(it.next());
         }
-        for (int i = 0; i < 2; i++) {
-            try {
-                preThreads.get(i).join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        writer.close();
+    }
+
+    private static void pushToMainThread(ConcurrentHashMap<String,String> map, Set<String> allStr) throws IOException {
         for (Map.Entry<String,String> ent: map.entrySet()){
             System.out.println(ent.getKey() + ": " + ent.getValue());
         }
@@ -50,13 +49,24 @@ public class Main {
         System.out.println("\n\n\n##########################################################################" +
                 "\n################################## Finishing!!! ##########################\n\n\n");
         printToCSV("output.csv",allStr);
+        allStr.clear();
+        map.clear();
     }
 
-    public static void printToCSV(String file, Set<String> strs) throws IOException{
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        Iterator<String> it = strs.iterator();
-        while (it.hasNext()){
-            writer.write(it.next());
+    private static void startPreThread(int start, int step, ConcurrentHashMap<String,String> map){
+        ArrayList<Thread> preThreads = new ArrayList<Thread>();
+        for (int i = start; i < start+step; i++) {
+            preThreads.add(new Thread(new PreThread(i,map)));
+        }
+        for (int i = 0; i < step; i++) {
+            preThreads.get(i).start();
+        }
+        for (int i = 0; i < step; i++) {
+            try {
+                preThreads.get(i).join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
